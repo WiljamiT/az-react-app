0003e24bc51422b3c53d5279dd36492a5cec728d
WiljamiT <74732018+WiljamiT@users.noreply.github.com>
2024-01-24 18:57:16 +0200

    login delete

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Create from "./pages/Create";
-import "./App.css";
-import text from "./strings.json";
-import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import { useState } from "react";
+import './App.css';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userName, setUserName] = useState("");
-
-  const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/" />;
-  };
-
   return (
-    <Router>
-      <nav className="navbar">
-        <Link to="/" className="navbar_title">
-          <h1>{text.strings.header.nav}</h1>
-          <h5>Hello: <i>{!userName ? "-" : userName}</i></h5>
-        </Link>
-        <div className="navbar_links">
-          <Link to="/" className="navbar_link">
-            {text.strings.button.home}
-          </Link>
-          <Link to="/create" className="navbar_link">
-            {text.strings.button.share}
-          </Link>
-
-          <div className="google_btn">
-            <GoogleLogin
-              onSuccess={(credentialResponse) => {
-                const decoded = jwtDecode(credentialResponse?.credential);
-                console.log(decoded);
-                setIsAuthenticated(true);
-                setUserName(`${decoded.name}`);
-              }}
-              onError={() => {
-                console.log('LOGIN FAILED');
-              }}
-            />
-          </div>
-        </div>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home isAuthenticated={isAuthenticated} />} />
-        <Route
-          path="/create"
-          element={<PrivateRoute element={<Create />} />}
-        />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>Testi</h1>
+      </header>
+    </div>
   );
 }
 
